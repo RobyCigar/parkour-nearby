68dd6f81b4140dd72649773510e146d7492399f5
HP <rabihutomo11@gmail.com>
2021-06-06 19:40:58 +0700

    fix type

--- components/ChangeView.tsx
+++ components/ChangeView.tsx
@@ -1,6 +1,6 @@
 import { useMap } from 'react-leaflet'
 
-function ChangeView({ center, zoom }) {
+function ChangeView({ center, zoom }: {center: number, zoom: number}) {
   const map = useMap();
   map.setView(center, zoom);
   return null;
